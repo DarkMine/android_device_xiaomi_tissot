30969c504e37407417c72137482c68868db634cd
DarkMine <hknhzl@gmail.com>
2022-03-17 08:49:25 +0300

    CherisOS

--- cherish_tissot.mk
+++ cherish_tissot.mk
@@ -8,20 +8,27 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common CherishOS stuff.
+$(call inherit-product, vendor/cherish/config/common_full_phone.mk)
 
 # Inherit from tissot device
 $(call inherit-product, device/xiaomi/tissot/device.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := tissot
-PRODUCT_NAME := lineage_tissot
+PRODUCT_NAME := cherish_tissot
 BOARD_VENDOR := Xiaomi
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Mi A1
 PRODUCT_MANUFACTURER := Xiaomi
 TARGET_VENDOR := Xiaomi
+
+# CheriahOS  Stuffs
+CHERISH_BUILD_TYPE := UNOFFICIAL
+
+# Maintainer
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.cherish.maintainer=Lafactorial \
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
